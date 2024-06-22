de4aa605173dedc774e0e459059b403272640b0d
Pavithra-bcg <pavithrat23bcg038@skasc.ac.in>
2024-06-22 16:10:28 +0530

    Add files via upload

---
+++ task10/task.js
@@ -0,0 +1,10 @@
+function multiplyby(){
+    num1=document.getElementById("num1");
+    num2=document.getElementById("num2");
+  document.getElementById("result").innerHTML=num1*num2;
+}
+function divideby(){
+    num1=document.getElementById("num1");
+    num2=document.getElementById("num2");
+  document.getElementById("result").innerHTML=num1/num2;
+}
